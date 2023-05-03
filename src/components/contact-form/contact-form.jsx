ef97ce10067319a59a25c09130b3823b5fc4d469
Ida Hay Jørgensen <idahayjorgensen@idas-macbook-air.home>
2023-05-03 21:07:26 +0200

    add form validation

--- src/components/contact-form/contact-form.jsx
+++ src/components/contact-form/contact-form.jsx
@@ -8,16 +8,21 @@ export default function ContactForm() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const formData = new FormData(event.target);
-    const result = {
-      name: formData.get("name"),
-      mobile: formData.get("mobile"),
-      email: formData.get("email"),
-      zipcode: formData.get("zipcode"),
-      city: formData.get("city"),
-    };
-    console.log(result);
-    setShowModal(true);
+    const form = event.target;
+    if (form.checkValidity()) {
+      const formData = new FormData(form);
+      const result = {
+        name: formData.get("name"),
+        mobile: formData.get("mobile"),
+        email: formData.get("email"),
+        zipcode: formData.get("zipcode"),
+        city: formData.get("city"),
+      };
+      console.log(result);
+      setShowModal(true);
+    } else {
+      form.reportValidity();
+    }
   };
 
   const handleCloseModal = () => {
@@ -30,23 +35,23 @@ export default function ContactForm() {
       <form onSubmit={handleSubmit} ref={formRef}>
         <div className="form-section form-section--wide">
           <label htmlFor="name">Navn</label>
-          <input type="text" id="name" name="name" />
+          <input type="text" id="name" name="name" required />
         </div>
         <div className="form-section">
           <label htmlFor="mobile">Mobil</label>
-          <input type="tel" id="mobile" name="mobile" />
+          <input type="tel" id="mobile" name="mobile" required />
         </div>
         <div className="form-section">
           <label htmlFor="email">e-mail</label>
-          <input type="email" id="email" name="email" />
+          <input type="email" id="email" name="email" required />
         </div>
         <div className="form-section">
           <label htmlFor="zipcode">Postnr.</label>
-          <input type="text" id="zipcode" name="zipcode" />
+          <input type="text" id="zipcode" name="zipcode" required />
         </div>
         <div className="form-section">
           <label htmlFor="city">By</label>
-          <input type="text" id="city" name="city" />
+          <input type="text" id="city" name="city" required />
         </div>
         <div className="form-section form-section--wide form-section--btn">
           <button type="submit">Ring mig op</button>
